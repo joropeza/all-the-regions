5e253fb9c7a7bdef3c737fa5c63ef6e61b9b5d49
Jonny <joropeza@gmail.com>
2016-12-31 13:34:52 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _regions = require('./regions');
+
+var _regions2 = _interopRequireDefault(_regions);
+
+var _subregions = require('./subregions');
+
+var _subregions2 = _interopRequireDefault(_subregions);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = { regions: _regions2.default, subregions: _subregions2.default };
